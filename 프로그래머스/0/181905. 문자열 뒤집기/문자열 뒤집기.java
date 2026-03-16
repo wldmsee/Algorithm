3f0a9775b148d2d79101895b4db664e0742e047b
wldmsee <melobeat789@gmail.com>
2026-03-16 17:10:12 +0900

    [level 0] Title: 문자열 뒤집기, Time: 23.60 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        
+        String front = my_string.substring(0,s);
+        String middle = my_string.substring(s, e+1);
+        String end = my_string.substring(e+1);
+
+        String reversed = new StringBuilder(middle).reverse().toString();
+        return front + reversed + end;
+    }
+}
